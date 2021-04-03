1d4b01c1637ac72a58e25a11e3e846ac920823a1
PrzemyslawZnojek <przemek.znojek@gmail.com>
2021-04-03 23:45:40 +0200

    Init commit

---
+++ out-calories-core/src/main/java/pl/rhino/code/OutCaloriesCoreApplication.java
@@ -0,0 +1,13 @@
+package pl.rhino.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutCaloriesCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutCaloriesCoreApplication.class, args);
+	}
+
+}
---
+++ out-calories-core/src/test/java/pl/rhino/code/OutCaloriesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rhino.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutCaloriesCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
