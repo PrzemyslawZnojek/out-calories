8d07bece57ff5970670edf3ff4ba3051640a3a47
PrzemyslawZnojek <przemek.znojek@gmail.com>
2021-04-14 19:13:29 +0200

    Add Company and Meal models.

---
+++ out-calories-core/src/main/java/pl/rhino/code/model/Company.java
@@ -0,0 +1,6 @@
+package pl.rhino.code.model;
+
+public class Company {
+    long id;
+    String companyName;
+}
---
+++ out-calories-core/src/main/java/pl/rhino/code/model/Meal.java
@@ -0,0 +1,11 @@
+package pl.rhino.code.model;
+
+public class Meal {
+    long id;
+    long companyId;
+    String name;
+    int kcalAmount;
+    int proteins;
+    int carbohydrates;
+    int fats;
+}
