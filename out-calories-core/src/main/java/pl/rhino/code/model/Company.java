65323a592b0f04c615f4a61fde4a187cf9aeb8c9
PrzemyslawZnojek <przemek.znojek@gmail.com>
2021-04-14 22:45:03 +0200

    Add dependency for lombok and springboot jpa.

--- out-calories-core/src/main/java/pl/rhino/code/model/Company.java
+++ out-calories-core/src/main/java/pl/rhino/code/model/Company.java
@@ -1,6 +1,13 @@
 package pl.rhino.code.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
 public class Company {
-    long id;
-    String companyName;
+    private long id;
+    private String companyName;
+    private CompanyAddress address;
 }
--- out-calories-core/src/main/java/pl/rhino/code/model/Meal.java
+++ out-calories-core/src/main/java/pl/rhino/code/model/Meal.java
@@ -1,11 +1,17 @@
 package pl.rhino.code.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
 public class Meal {
-    long id;
-    long companyId;
-    String name;
-    int kcalAmount;
-    int proteins;
-    int carbohydrates;
-    int fats;
+    private long id;
+    private long companyId;
+    private String name;
+    private int kcalAmount;
+    private int proteins;
+    private int carbohydrates;
+    private int fats;
 }
