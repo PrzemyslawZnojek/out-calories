f9b794bf960d3eb3824a78031a732684cc4f55d0
PrzemyslawZnojek <przemek.znojek@gmail.com>
2021-04-28 00:13:43 +0200

    Add methods methods to retrive by id and to create object.

--- out-calories-core/src/main/java/pl/rhino/code/resource/CompanyAddressResource.java
+++ out-calories-core/src/main/java/pl/rhino/code/resource/CompanyAddressResource.java
@@ -1,4 +1,30 @@
 package pl.rhino.code.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.rhino.code.model.Company;
+import pl.rhino.code.model.CompanyAddress;
+import pl.rhino.code.service.interfaces.ICompanyAddressService;
+
 public class CompanyAddressResource {
+
+    ICompanyAddressService service;
+
+    @Autowired
+    public CompanyAddressResource(ICompanyAddressService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public CompanyAddress findById(@PathVariable("id") Long id){
+        return service.findById(id);
+    }
+
+    @PostMapping("create")
+    public void createCompanyAddress(@RequestBody CompanyAddress companyAddress){
+        service.createCompanyAddress(companyAddress);
+    }
 }
--- out-calories-core/src/main/java/pl/rhino/code/service/implementations/CompanyAddressService.java
+++ out-calories-core/src/main/java/pl/rhino/code/service/implementations/CompanyAddressService.java
@@ -2,6 +2,7 @@ package pl.rhino.code.service.implementations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.rhino.code.dao.interfaces.ICompanyAddressDao;
+import pl.rhino.code.model.CompanyAddress;
 import pl.rhino.code.service.interfaces.ICompanyAddressService;
 
 public class CompanyAddressService implements ICompanyAddressService {
@@ -11,5 +12,15 @@ public class CompanyAddressService implements ICompanyAddressService {
     @Autowired
     public CompanyAddressService(ICompanyAddressDao companyAddressDao) {
         this.companyAddressDao = companyAddressDao;
+    }
+
+    @Override
+    public CompanyAddress findById(Long id) {
+        return companyAddressDao.findById(id);
+    }
+
+    @Override
+    public void createCompanyAddress(CompanyAddress companyAddress) {
+        companyAddressDao.createCompanyAddress(companyAddress);
     }
 }
