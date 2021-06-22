218018f450334e3c8d24158180fcd4157de27c25
PrzemyslawZnojek <przemek.znojek@gmail.com>
2021-06-22 22:24:11 +0200

    Make DDL as none.

--- out-calories-core/src/test/java/pl/rhino/code/integration/tests/dao/CompanyDaoTest.java
+++ out-calories-core/src/test/java/pl/rhino/code/integration/tests/dao/CompanyDaoTest.java
@@ -1,13 +1,10 @@
 package pl.rhino.code.integration.tests.dao;
 
-import org.junit.ClassRule;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -19,12 +16,10 @@ import pl.rhino.code.model.Company;
 import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 @Testcontainers
 @SpringBootTest(classes = OutCaloriesCoreApplication.class)
-@ActiveProfiles({"tc", "tc-auto"})
 public class CompanyDaoTest {
 
     @Container
@@ -43,10 +38,5 @@ public class CompanyDaoTest {
     void findByIdShouldEndWithSuccess() {
         Company byId = companyDao.findById(1L);
         assertEquals(byId.getId(), 1L);
-    }
-
-    void insertCompany() {
-        Company company = Company.builder().id(1L).name("Company1").build();
-        companyDao.createCompany(company);
     }
 }
